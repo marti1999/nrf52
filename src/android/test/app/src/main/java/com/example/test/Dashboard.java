a6b7ea4112dd1684be9c05e00964f83e5439c847
Martí Caixal <mcaixal1999@gmail.com>
2022-12-02 20:51:16 +0100

    cleaning code and commenting

--- src/android/test/app/src/main/java/com/example/test/Dashboard.java
+++ src/android/test/app/src/main/java/com/example/test/Dashboard.java
@@ -1,8 +1,8 @@
 package com.example.test;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
@@ -13,11 +13,8 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
-
 import com.clj.fastble.BleManager;
 import com.clj.fastble.callback.BleGattCallback;
 import com.clj.fastble.callback.BleReadCallback;
@@ -32,13 +29,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Dashboard extends AppCompatActivity {
+
+    // Activity layout components
     private TextView ResultsTemp, ResultsPrec, ResultsWind;
     private TextView LineResults;
     private Button btnScan, btnConnect;
@@ -47,9 +43,9 @@ public class Dashboard extends AppCompatActivity {
     private static final String CHARACTERISTIC_UUID = "a89b4483df7f4539ab8ae6bfb4070640";
     private boolean isNRF52Found = false;
     private boolean isNRF52Connected = false;
+
     // creating a variable for our Firebase Database.
     FirebaseDatabase firebaseDatabase;
-
     // creating a variable for our Database reference for Firebase.
     DatabaseReference databaseReference;
 
@@ -59,10 +55,12 @@ public class Dashboard extends AppCompatActivity {
     ValueEventListener asyncListenerAll, asyncListenerLast;
     BleDevice nrf52;
 
+    // handler used for BLE connection
     Handler handler = new Handler();
     Runnable handler_runnable;
     int handler_delay = 5000;
 
+    // making sure to disable all BLE, handler and asynk task before destroying activity
     @Override
     protected void onDestroy() {
         disableFetchAllEntriesAsync();
@@ -71,6 +69,7 @@ public class Dashboard extends AppCompatActivity {
         if (BleManager.getInstance().isConnected(nrf52)) {
             BleManager.getInstance().disconnect(nrf52);
         }
+        handler.removeCallbacks(handler_runnable);
         BleManager.getInstance().destroy();
     }
 
@@ -79,10 +78,12 @@ public class Dashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         getSupportActionBar().hide();
+
         // creating firebase instance
         firebaseDatabase = FirebaseDatabase.getInstance();
         // Creating a reference to our collection
         databaseReference = firebaseDatabase.getReference("Entries");
+
         //Initializing and mapping GUI components
         ResultsTemp = findViewById(R.id.temp_value);
         ResultsPrec = findViewById(R.id.precipitation_value);
@@ -108,12 +109,13 @@ public class Dashboard extends AppCompatActivity {
         fetchLastEntryAsync();
     }
 
+    // restarting everything that has been paused
     @Override
     protected void onResume(){
         fetchLastEntryAsync();
         handler.postDelayed(handler_runnable = new Runnable() {
             public void run() {
-//                Log.d("HANDLER", "handler executed");
+                // Log.d("HANDLER", "handler executed");
                 if (isNRF52Connected){
                     ble_read();
                 }
@@ -123,6 +125,7 @@ public class Dashboard extends AppCompatActivity {
         super.onResume();
     }
 
+    // pausing background tasks and similar
     @Override
     protected void onPause() {
         disableFetchLastEntryAsync();
@@ -131,10 +134,9 @@ public class Dashboard extends AppCompatActivity {
 
     }
 
-    // fetches last entry by using the key previously saved.
+    // fetches last entry asynchronous, listener executed whenever there are changes on the DB
     private void fetchLastEntryAsync() {
-        // notice that child(lastKey) is called before get(). Now it only gets the element inside the list, instead of the whole list.
-        // Alternatively it could be done by lastKey variable.
+        // asyncListenerLast variable needs to be outside the inner function because it is needed somewhere else.
         asyncListenerLast = databaseReference.orderByKey().limitToLast(1).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -147,19 +149,19 @@ public class Dashboard extends AppCompatActivity {
                 ResultsPrec.setText(en.getPrecipitation() + " mL");
                 ResultsWind.setText(en.getWind() + " km/h");
 
-                Log.e("JÚLIA", en.toString());
+                // Log.e("JÚLIA", en.toString());
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                Log.e("FIREBASE", "Error getting data", error.toException().getCause());
             }
         });
-
     }
 
+    // fetches all entries asynchronous, listener executed whenever there are changes on the DB
     private void fetchAllEntriesAsync() {
-        // this variable needs to be outside the function because it is needed in another.
+        // asyncListenerAll variable needs to be outside the inner function because it is needed somewhere else.
         asyncListenerAll = databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -169,7 +171,6 @@ public class Dashboard extends AppCompatActivity {
                     Entry en = entrySnap.getValue(Entry.class);
                     allEntries.add(en);
                 }
-                //Results.setText(String.valueOf(allEntries));
             }
 
             @Override
@@ -179,6 +180,7 @@ public class Dashboard extends AppCompatActivity {
         });
     }
 
+    // checking BLE permissions before actually trying to connect
     private void ble_checkpermission() {
         // https://developer.android.com/guide/topics/connectivity/bluetooth/permissions
         // https://www.geeksforgeeks.org/android-how-to-request-permissions-in-android-application/
@@ -194,15 +196,16 @@ public class Dashboard extends AppCompatActivity {
 
         ActivityCompat.requestPermissions(Dashboard.this, new String[]{Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.ACCESS_FINE_LOCATION}, BLUETOOTH_CODE);
 
-//        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT)
-//                != PackageManager.PERMISSION_GRANTED)
-//        {
-//            Log.e("ble", "NO TÉ PERMISOS");
-//            Toast.makeText(this, "error permisos BLUETOOTH_CONNECT", Toast.LENGTH_SHORT).show();
-//            return;
-//        }
+        if(ContextCompat.checkSelfPermission(Dashboard.this, Manifest.permission.BLUETOOTH_CONNECT)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            Log.e("BLE", "Permissions are not granted");
+            Toast.makeText(this, "You need to allow BLE permissions", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 
+    // executed when the users accepts/denies permissions request
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String[] permissions,
@@ -213,15 +216,16 @@ public class Dashboard extends AppCompatActivity {
 
         if (requestCode == BLUETOOTH_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                Toast.makeText(MainActivity.this, "BLUETOOTH_CONNECT_CODE Permission Granted", Toast.LENGTH_SHORT) .show();
                 ble_setScanRule();
                 ble_startScan();
             } else {
-                Toast.makeText(Dashboard.this, "permisos denegats", Toast.LENGTH_SHORT).show();
+                Toast.makeText(Dashboard.this, "Permissions have been denied", Toast.LENGTH_SHORT).show();
+                Log.d("PERMISSIONS", "permissions denied by the user");
             }
         }
     }
 
+    // to make things easier, we are only scanning for our device by filtering its physical address
     private void ble_setScanRule() {
         BleScanRuleConfig scanRuleConfig = new BleScanRuleConfig.Builder()
                 .setServiceUuids(null)
@@ -233,6 +237,7 @@ public class Dashboard extends AppCompatActivity {
         BleManager.getInstance().initScanRule(scanRuleConfig);
     }
 
+    // Once scan rules have been applied, a BLE scan is started
     private void ble_startScan() {
 
         if (isNRF52Connected || isNRF52Found) {
@@ -268,6 +273,7 @@ public class Dashboard extends AppCompatActivity {
 
     }
 
+    // connects to the device
     private void ble_connect() {
         if (!isNRF52Found) {
             Toast.makeText(this, "NRF52 was not discovered. Scan again", Toast.LENGTH_SHORT).show();
@@ -284,7 +290,7 @@ public class Dashboard extends AppCompatActivity {
 
                 @Override
                 public void onConnectFail(BleDevice bleDevice, BleException exception) {
-                    Log.d("BLE", "connexion failed");
+                    Log.e("BLE", "connexion failed");
                 }
 
                 @Override
@@ -293,18 +299,11 @@ public class Dashboard extends AppCompatActivity {
                     isNRF52Connected = true;
                     Toast.makeText(Dashboard.this, "Connected", Toast.LENGTH_SHORT).show();
                     LineResults.setText("");
-
-                    // TODO mostrar per pantalla els serveis i característiques
-
-//                    List<BluetoothGattService> services = BleManager.getInstance().getBluetoothGattServices(nrf52);
-//                    services.
-
                 }
 
                 @Override
                 public void onDisConnected(boolean isActiveDisConnected, BleDevice device, BluetoothGatt gatt, int status) {
                     Log.d("BLE", "Disconnected ");
-
                 }
             });
         } else {
@@ -312,8 +311,8 @@ public class Dashboard extends AppCompatActivity {
         }
     }
 
+    // read data from BLE device
     private void ble_read() {
-
         if (!isNRF52Connected) {
             return;
         }
@@ -324,7 +323,6 @@ public class Dashboard extends AppCompatActivity {
         BleManager.getInstance().read(nrf52, uuid_service, uuid_characteristic, new BleReadCallback() {
             @Override
             public void onReadSuccess(byte[] data) {
-
                 String big_endian = HexUtil.formatHexString(data, false);
                 String little_endian = swapEndianString(big_endian);
                 String temperature = little_endian.substring(6, 8);
@@ -381,8 +379,8 @@ public class Dashboard extends AppCompatActivity {
         pushedRef.setValue(entry).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void unused) {
-                //clearTextBoxes();
                 lastKey = pushedRef.getKey();
+                Log.d("FIREBASE", "Entry added to firebase");
             }
         });
     }
